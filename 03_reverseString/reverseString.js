5b788d73b3efe75effbf331ae248509b5759698f
awesome13524 <94251418+awesome13524@users.noreply.github.com>
2023-08-18 12:40:39 -0400

    Complete exercises 1 through 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    const characters = string.split("");
+    for(i = characters.length - 1; i >= 0; i--){
+        newString += characters[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+    for(i = 0; i < args.length; i++){
+        if(myArray.includes(args[i])){
+            myArray.splice(myArray.indexOf(args[i]), 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
